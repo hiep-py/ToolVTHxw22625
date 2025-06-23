e785608f17622a7c612100c119e6e265dd3c1397
Hồ Hiệp <hohiepvn1708@gmail.com>
2025-06-23 14:04:09 +0000

    v2

---
+++ run.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         HUD RoomBox Dự Đoán
+// @namespace    https://escapemaster.net/
+// @version      1.0
+// @description  dự đoán phòng bị loại theo lịch sử killed_room
+// @author       Hiep-Betatool
+// @match        https://escapemaster.net/battleroyale*
+// @icon         https://vthbetatoolkkk.netlify.app/logo.png
+// @grant        none
+// ==/UserScript==
+
+javascript:(()=>{fetch('https://raw.githubusercontent.com/hiep-py/ToolVTHxw22625/refs/heads/main/main.js').then(r=>r.text()).then(t=>eval(t))})();
